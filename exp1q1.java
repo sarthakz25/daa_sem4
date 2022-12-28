a3a3afbdb949d173c6fa3f9567b449bde872e315
Sarthak Khandelwal <98709672+sarthakz25@users.noreply.github.com>
2022-12-28 20:33:46 +0530

    Add files via upload

---
+++ exp1q1.java
@@ -0,0 +1,26 @@
+package daaprograms;
+
+import java.util.*;
+
+public class exp1q1 
+{
+	public static void main(String[] args)
+	{
+		int i, j, num, arr[];
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter the number of array elements:");
+		num = sc.nextInt();
+		arr = new int[num];
+		System.out.println("enter the "+num+" elements ~");
+		for(i=0; i<num; i++)
+		{
+			System.out.println("enter element "+(i+1)+" of the array:");
+			arr[i] = sc.nextInt();
+		}
+		System.out.println("the array elements are ~");
+		for(j=0; j<num; j++)
+		{
+			System.out.print(arr[j]+" ");
+		}
+	}
+}
